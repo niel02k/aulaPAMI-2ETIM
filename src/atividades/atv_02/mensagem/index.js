c734affb978d8f3d9c0d4aa0aaef1220f306f080
marcos <nielfreitasfree@gmail.com>
2024-11-08 09:41:35 -0300

    08-11

--- src/atividades/atv_02/mensagem/index.js
+++ src/atividades/atv_02/mensagem/index.js
@@ -1,12 +1,17 @@
-import { Text } from 'react-native';
+import { Text, View} from 'react-native';
 
 import styles from './styles';
 
-export default function Mensagem(props) {
+export default function Mensagem({nome, idade}) {
     return (
+        <View style={styles.container}>
         <Text style={styles.texto}>
-            Olá {props.nome + '\n' + 'idade:' + props.idade} 
+            Olá {nome } 
         </Text>
+        <Text style={styles.idade}>
+            idade:{idade} 
+        </Text>
+        </View>
     );
 }
 
--- src/atividades/atv_02/mensagem/styles.js
+++ src/atividades/atv_02/mensagem/styles.js
@@ -1,12 +1,24 @@
 import { StyleSheet } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 const styles = StyleSheet.create({ 
     texto: {
         fontSize: 25, 
-        alignItems:'center',
-        textAlign:'center'
+        fontWeight: 'bold',
+        
     }, 
+    idade:{
+        fontSize: RFValue (20), 
 
+    },
+
+    container :{
+           marginBottom:(5),
+           borderBottomWidth:(2),
+           width:'100%',
+           alignItems:'center'
+
+    }
 });
 
 export default styles;
